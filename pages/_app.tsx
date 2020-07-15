4ac207213552de4fc3722ea3a0600fc9b9a9b2cc
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-15 00:03:39 -0500

    web vitals added to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
 
+export function reportWebVitals(metric: any) {
+	console.log(metric);
+}
+
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
