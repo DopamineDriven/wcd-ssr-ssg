78c41c143e6ad172fe6e37409c8bfe6b15075cb3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-16 17:04:51 -0500

    built out index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import { Fragment } from "react";
+import Head from "next/head";
+import Post from "../types/posts";
+import { getAllPosts } from "../lib/api";
+import { CMS_NAME } from "../lib/constants";
+import Container from "../components/container";
+import MorePosts from "../components/morePosts";
+import HeroPost from "../components/heroPost";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
 
-const Index = () => {
+const Index = ({ allPosts }: { allPosts: Post[] }) => {
+	const heroPost = allPosts[0];
+	const morePosts = allPosts.slice(1);
 	return (
-		<div>
-			<h1>Up and Running</h1>
-		</div>
+		<Fragment>
+			<Layout>
+				<Head>
+					<title>SSG via Next.js and {CMS_NAME}</title>
+				</Head>
+				<Container>
+					<Intro />
+					{heroPost && (
+						<HeroPost
+							title={heroPost.title}
+							coverImage={heroPost.coverImage}
+							date={heroPost.date}
+							author={heroPost.author}
+							slug={heroPost.slug}
+							excerpt={heroPost.excerpt}
+						/>
+					)}
+					{morePosts.length > 0 && <MorePosts posts={morePosts} />}
+				</Container>
+			</Layout>
+		</Fragment>
 	);
 };
 
 export default Index;
+
+export const getStaticProps = async () => {
+	const allPosts = getAllPosts([
+		"title",
+		"date",
+		"slug",
+		"author",
+		"coverImage",
+		"excerpt"
+	]);
+	return {
+		props: { allPosts }
+	};
+};
