98979779d14574aec7715a681693d72f868f50ad
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-16 16:50:41 -0500

    built out [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import ErrorPage from "../_error";
 import Container from "../../components/container";
@@ -11,6 +13,7 @@ import Head from "next/head";
 import { CMS_NAME } from "../../lib/constants";
 import mdToHtml from "../../lib/mdToHtml";
 import PostType from "../../types/posts";
+import markdownToHtml from "../../lib/mdToHtml";
 
 const Post = ({
 	post,
@@ -26,7 +29,70 @@ const Post = ({
 		<ErrorPage statusCode={404} />
 	) : (
 		<Layout preview={preview}>
-            
-        </Layout>
+			<Container>
+				<Header />
+				{router.isFallback ? (
+					<PostTitle>🐜🐜🐜Loading🐜🐜🐜</PostTitle>
+				) : (
+					<Fragment>
+						<article className="mb-32">
+							<Head>
+								<title>
+									{post.title} | SSG via Next.js and {CMS_NAME}
+								</title>
+							</Head>
+							<PostHeader
+								title={post.title}
+								coverImage={post.coverImage}
+								date={post.date}
+								author={post.author}
+							/>
+							<PostBody content={post.content} />
+						</article>
+					</Fragment>
+				)}
+			</Container>
+		</Layout>
 	);
+};
+
+export default Post;
+
+export const getStaticProps = async ({
+	params
+}: GetStaticProps & { params: { slug: string } }) => {
+	const post = getPostBySlug(params.slug, [
+		"title",
+		"date",
+		"slug",
+		"author",
+		"content",
+		"opImage",
+		"coverImage"
+	]);
+	const content = await markdownToHtml(post.content || "");
+
+	return {
+		props: {
+			post: {
+				...post,
+				content
+			}
+		}
+	};
+};
+
+export const getStaticPaths = async ({}: GetStaticPaths) => {
+	const posts = getAllPosts(["slug"]);
+
+	return {
+		paths: posts.map(posts => {
+			return {
+				params: {
+					slug: posts.slug
+				}
+			};
+		}),
+		fallback: false
+	};
 };
